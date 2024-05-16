584b11b08bf4e65a1e9adc1628ad29ec79b67323
Nandantelkark <nandan.telkar@techonesky.com>
2024-05-16 15:20:13 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+
+import express  from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (_req , res) =>   {
+    res.send('hello world');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}
+);
